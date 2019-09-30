b0856fbc48b51f386ba106f7e3ed4415737971d5
KaiHe <695909844@qq.com>
2019-09-30 21:55:34 +0800

    day6

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -40,6 +40,7 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 				if(response.success){
 					alert("添加成功");
 					$scope.entity={};
+					location.reload()
 					editor.html("");
 				}else{
 					alert(response.message);
@@ -109,9 +110,9 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 	}
 
 	$scope.$watch("entity.goods.cateGory1Id",function (newValue,oldValue) {
-		$scope.itemCat3List = null;
-		$scope.entity.goods.typTemplateId = null;
 		if (newValue!=null) {
+			$scope.itemCat3List = null;
+			$scope.entity.goods.typeTemplateId = null;
 			itemCatService.findByParentId(newValue).success(
 				function (response) {
 					$scope.itemCat2List = response;
@@ -121,8 +122,8 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 	})
 
 	$scope.$watch("entity.goods.cateGory2Id",function (newValue,oldValue) {
-		$scope.entity.goods.typTemplateId = null;
 		if (newValue!=null) {
+			$scope.entity.goods.typeTemplateId = null;
 			itemCatService.findByParentId(newValue).success(
 				function (response) {
 					$scope.itemCat3List = response;
@@ -135,17 +136,19 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 		if (newValue!=null) {
 			itemCatService.findOne(newValue).success(
 				function (response) {
-					$scope.entity.goods.typTemplateId = response.typeId;
+					$scope.entity.goods.typeTemplateId = response.typeId;
 				}
 			)
 		}
 	})
 
 
-	$scope.$watch("entity.goods.typTemplateId",function (newValue,oldValue) {
+	$scope.$watch("entity.goods.typeTemplateId",function (newValue,oldValue) {
 		if (newValue!=null) {
+
 			typeTemplateService.findOne(newValue).success(
 				function (response) {
+
 					$scope.brandList =JSON.parse(response.brandIds);
 					$scope.entity.goodsDesc.customAttributeItems = JSON.parse(response.customAttributeItems)
 				}
@@ -157,4 +160,81 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 			)
 		}
 	})
+
+	//记录规格勾选数组
+	$scope.entity.goodsDesc.specificationItems = []
+
+	$scope.createSpecificationItems = function ($event,text,optionName) {
+
+		var list = $scope.searchObjectByKey($scope.entity.goodsDesc.specificationItems,'attributeName', text);
+
+		if (list != null) {
+			//表示不是第一次点击还有其他值
+			if ($event.target.checked) {
+				list.attributeValue.push(optionName)
+			}else {
+				//取消勾选
+				list.attributeValue.splice(list.attributeValue.indexOf(optionName),1)
+
+				if (list.attributeValue.length == 0){
+					//如果全部取消了 就吧specificationItems中的这一整条记录移除
+					$scope.entity.goodsDesc.specificationItems.splice($scope.entity.goodsDesc.specificationItems.indexOf(list),1)
+				}
+			}
+		}else {
+			//第一次点击 没有其他值
+			if ($event.target.checked) {
+					$scope.entity.goodsDesc.specificationItems.push({attributeName:text,attributeValue:[optionName]})
+			}
+		}
+
+	}
+ //查询specificationItems集合中有没有text
+	$scope.searchObjectByKey = function (list,key,text) {
+		for (var i = 0;i<list.length;i++){
+			if (list[i][key]==text) {
+				return list[i];
+			}
+		}
+		return null;
+	}
+
+
+	//增加sku 商品列表
+	$scope.createItems = function () {
+		//初始化sku 商品列表
+		$scope.entity.items=[ {spec:{} ,price:0,num:99999,status:'0',isDefault:'0' } ];
+		//根据这个集合来创建 sku 商品
+		 var Items = $scope.entity.goodsDesc.specificationItems;
+
+		for(var i = 0;i<Items.length;i++){
+			$scope.entity.items = $scope.addColumn($scope.entity.items,Items[i].attributeName,Items[i].attributeValue)
+		}
+	}
+
+
+	$scope.addColumn = function (list,attributeName,attributeValue) {
+		var newList=[] ;
+		for(var i = 0; i<list.length; i++){
+
+			for (var j = 0;j<attributeValue.length;j++){
+				var oldRow = list[i];
+
+				var newRow = JSON.parse(JSON.stringify(oldRow));
+
+				newRow.spec[attributeName] = attributeValue[j];
+
+				newList.push(newRow)
+			}
+		}
+		return newList;
+	}
+
+
+	//清空数组
+	$scope.cleanList = function () {
+		$scope.entity.goodsDesc.specificationItems = [];
+		$scope.entity.itemList=[ {spec:{} ,price:0,num:99999,status:'0',isDefault:'0' } ]
+	}
+
 });	
